e8cdc0c61fef110407cda4337bcf9d2a47fc8e87
Olivier Monnier <omonnier.dev@gmail.com>
2017-10-19 10:15:48 +0200

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "Mastodonte",
+      script    : "app.js",
+      watch: true,
+      ignore_watch: [
+        "node_modules"
+      ],
+      max_memory_restart: "20M",
+      out_file: "logs/out.log",
+      error_file: "logs/err.log",
+      env: {
+        NODE_ENV: "development",
+        PORT: "8080"
+      },
+      env_production : {
+        NODE_ENV: "production",
+        PORT: "8000"
+      }
+    }
+  ]
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  /*deploy : {
+    production : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
+    },
+    dev : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }*/
+}
